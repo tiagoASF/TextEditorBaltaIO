1af59012d4ff14535f2e65f69ae50294d8054545
Tiago Fialho <tiago.asf@outlook.com>
2020-08-04 00:40:41 -0300

    Aplicação completada

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TextEditor
 {
@@ -6,7 +7,76 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Text Editor");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Editar arquivo");
+            Console.WriteLine("0 - Sair");
+
+            Console.Write("\nInforme a opção desejada: ");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.Write("Caminho do arquivo: ");
+            string path = Console.ReadLine(); 
+
+            using (StreamReader file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine();
+                Console.WriteLine(text);
+                Menu(); 
+            }
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o texto no campo abaixo (ESC para sair)");
+
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(text);
+            Menu();
+
+        }
+
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.Write("Informe o path para salvamento: ");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso");
+
         }
     }
 }
